0d18bc48c70bd7b42e95542f6e27c683b3e2e0ac
Alpesh <67307481+Alpeshpatel03434@users.noreply.github.com>
2023-01-02 01:11:53 +0530

    Update script.js

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,7 +1,9 @@
 /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ * Custom JavaScript
  */
+
 
 
 function addTOCart(SllerId, ProductId, ProductName, ProductPrice) {
